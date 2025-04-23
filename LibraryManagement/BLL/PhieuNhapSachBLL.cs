caa9608c955643deab31b63fdb20412005fee870
vantri206 <vantri0605@gmail.com>
2025-04-24 01:40:56 +0700

    add dal and logic for nhapsach

---
+++ LibraryManagement/BLL/PhieuNhapSachBLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManagement.DAL;
+using LibraryManagement.DTO;
+
+namespace LibraryManagement.BLL
+{
+    public class PhieuNhapSachBLL
+    {
+        private static PhieuNhapSachBLL instance;
+        public static PhieuNhapSachBLL Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new PhieuNhapSachBLL();
+                }
+                return instance;
+            }
+        }
+        public async Task<List<PHIEUNHAPSACH>> GetAllPhieuNhapSach()
+        {
+            return await PhieuNhapSachDAL.Instance.GetAllPhieuNhapSach();
+        }
+        public async Task<(bool, string)> AddPhieuNhapSach(PHIEUNHAPSACH pns)
+        {
+            pns.NgayNhap = DateTime.Now;
+            pns.TongTien = 0;
+            return await PhieuNhapSachDAL.Instance.AddPhieuNhapSach(pns);
+        }
+        public async Task<(bool, string)> UpdatePhieuNhapSach(PHIEUNHAPSACH pns)
+        {
+            var phieunhapsach = await PhieuNhapSachDAL.Instance.GetPhieuNhapSachById(pns.SoPNS);
+            if (phieunhapsach == null)
+            {
+                return (false, "KhÔng tìm thấy phiếu nhập");
+            }
+            if (pns.NgayNhap > DateTime.Now)
+            {
+                return (false, "Ngày nhập không hợp lệ");
+            }
+            if (pns.TongTien < 0)
+            {
+                return (false, "Tổng tiền không hợp lệ");
+            }
+            return await PhieuNhapSachDAL.Instance.UpdatePhieuNhapSach(pns);
+        }
+    }
+}
--- LibraryManagement/DAL/CT_PhieuNhapSachDAL.cs
+++ LibraryManagement/DAL/CT_PhieuNhapSachDAL.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LibraryManagement.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,40 @@ using System.Threading.Tasks;
 
 namespace LibraryManagement.DAL
 {
-    internal class CT_PhieuNhapSachDAL
+    public class CT_PhieuNhapSachDAL
     {
+        private static CT_PhieuNhapSachDAL instance;
+        public static CT_PhieuNhapSachDAL Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new CT_PhieuNhapSachDAL();
+                }
+                return instance;
+            }
+        }
+        public async Task<List<CT_PHIEUNHAPSACH>> GetAllCT_PhieuNhapSach()
+        {
+            using (var context = new LibraryManagementEntities())
+            {
+                return await context.CT_PHIEUNHAPSACH.ToListAsync();
+            }
+        }
+        public async Task<(bool, string)> AddCT_PhieuNhapSach(CT_PHIEUNHAPSACH ctpns)
+        {
+            using (var context = new LibraryManagementEntities())
+                try
+                {
+                    context.CT_PHIEUNHAPSACH.Add(ctpns);
+                    await context.SaveChangesAsync();
+                    return (true, "Thêm chi tiết phiếu nhập sách thành công");
+                }
+                catch(Exception ex)
+                {
+                    return (false, ex.Message);
+                }
+        }
     }
 }
