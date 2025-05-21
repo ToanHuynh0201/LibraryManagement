ebe5594f25fd1a7338c5af4c2367ddb6d1a60884
Ngô Quang Tiến <quangtienngo661@gmail.com>
2025-05-21 09:37:42 +0700

    add receipt page, window and report page, window

---
+++ LibraryManagement/View/Receipt/AddPenaltyReceiptWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagement.View.Receipt
+{
+    /// <summary>
+    /// Interaction logic for AddPenaltyReceiptWindow.xaml
+    /// </summary>
+    public partial class AddPenaltyReceiptWindow : Window
+    {
+        public AddPenaltyReceiptWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LibraryManagement/View/Receipt/PenaltyReceiptManagement.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryManagement.View.Receipt
+{
+    /// <summary>
+    /// Interaction logic for PenaltyReceiptManagement.xaml
+    /// </summary>
+    public partial class PenaltyReceiptManagement : Page
+    {
+        public PenaltyReceiptManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
